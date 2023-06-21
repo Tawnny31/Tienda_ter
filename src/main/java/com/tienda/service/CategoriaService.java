9b83f80ccf66efb015e1a94171935cc6d94c432f
Tawnny31 <105888154+Tawnny31@users.noreply.github.com>
2023-06-20 20:50:57 -0600

    Tienda-semana 6 Finalizado

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -8,5 +8,15 @@ public interface CategoriaService {
     
     //metodo para consultar las categorias. El parametro define si se muestran solo las activas o todas
     public List<Categoria> getCategorias(boolean activos);
+   
+    //Se obtiene una categoria por ID
+    public Categoria getCategoria(Categoria categoria);
+    
+    //insentar: cuando el idCategoria esta vacio (valor 0 o null)
+    //modificar: modifica cuando el idCategoria NO esta vacio
+    public void save(Categoria categoria);
+    
+    //eliminar: elimina un registro por idCategoria
+    public void delete(Categoria categoria);
     
 }
